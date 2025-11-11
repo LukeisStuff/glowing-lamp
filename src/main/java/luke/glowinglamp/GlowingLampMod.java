20a0ae0353e4424cfb0d7470f0fde294850eb0de
LukeisStuff <lukeisawesome03@gmail.com>
2025-11-10 20:48:02 -0500

    added mixin to make flat items rotate vertically, cleanup

--- src/main/java/luke/glowinglamp/GlowingLampMod.java
+++ src/main/java/luke/glowinglamp/GlowingLampMod.java
@@ -5,17 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
-import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
 public class GlowingLampMod implements ModInitializer, GameStartEntrypoint, ClientStartEntrypoint {
-    public static final String MOD_ID = "glowinglamp";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "glowinglamp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    @Override
-    public void onInitialize() {
-        LOGGER.info("GlowingLampMod initialized.");
-    }
+	@Override
+	public void onInitialize() {
+		LOGGER.info("GlowingLampMod initialized.");
+	}
 
 	@Override
 	public void beforeGameStart() {
---
+++ src/main/java/luke/glowinglamp/mixin/ItemModelStandardMixin.java
@@ -0,0 +1,34 @@
+package luke.glowinglamp.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.EntityRenderDispatcher;
+import net.minecraft.client.render.item.model.ItemModelStandard;
+import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(value = ItemModelStandard.class, remap = false)
+public class ItemModelStandardMixin {
+
+	@Inject(method = "renderAsItemEntity(Lnet/minecraft/client/render/tessellator/Tessellator;Lnet/minecraft/core/entity/Entity;Ljava/util/Random;Lnet/minecraft/core/item/ItemStack;IFFF)V",
+		at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glRotatef(FFFF)V", shift = At.Shift.AFTER))
+	private void addPitchBillboard(CallbackInfo ci) {
+		float pitch = EntityRenderDispatcher.instance.viewLerpPitch;
+		GL11.glRotatef(-pitch, 1.0F, 0.0F, 0.0F);
+	}
+
+	@Inject(method = "renderFlat", at = @At("HEAD"))
+	private void startGlow(CallbackInfo ci) {
+		GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
+		GL11.glDisable(GL11.GL_LIGHTING);
+	}
+
+	@Inject(method = "renderFlat", at = @At("RETURN"))
+	private void endGlow(CallbackInfo ci) {
+		GL11.glPopAttrib();
+	}
+}
